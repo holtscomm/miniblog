d8060f23266cd9cec673745fa898e84b261474a9
Graham Holtslander <menello@gmail.com>
2014-05-02 21:31:29 -0600

    Fixes #18

--- includes/miniblog.php
+++ includes/miniblog.php
@@ -21,46 +21,61 @@ if(!$database)
 
 $config = mb_config($database);
 
-$post	= (string) mysql_real_escape_string($_GET['post']);
-$page	= (int) mysql_real_escape_string(intval($_GET['page']));
+$post	= (string) $database->real_escape_string($_GET['post']);
+$page	= (int) $database->real_escape_string(intval($_GET['page']));
 $ppp	= (int) intval($config['posts-per-page']);
 $from	= (int) intval($ppp * $page);
+$category_name = (string) $database->real_escape_string($_GET['category']);
+echo $category_name;
+$category_id = $category_name != "" ? get_category_id($category_name, $database) : null;
 
+$sql = "SELECT * FROM `miniblog` WHERE `published` = 1";
 
-$sql = ($post == '') 
-            ? 'SELECT * FROM `miniblog` WHERE `published` = 1 ORDER BY `date` DESC LIMIT ' . $from . ', ' . $ppp 
-            : "SELECT * FROM `miniblog` WHERE `post_slug` = '{$post}' AND `published` = 1";
+if($category_id != null)
+{
+	$sql .= " AND `post_category` = {$category_id}";
+}
+if($post != '')
+{
+	$sql .= " AND `post_slug` = '{$post}'";
+}
+else
+{
+	$sql .= " ORDER BY `date` DESC LIMIT $from, $ppp";
+}
 
 $result = mb_query($sql, $database);
 
 $total_post_sql = "SELECT COUNT(*) FROM `miniblog` WHERE `published` = 1";
 $total_result = mb_query($total_post_sql, $database);
-$total = $total_result->field_seek(0);
+$total = $total_result->fetch_array()[0];
+
+$miniblog_posts = "";
 
 if($result->num_rows > 0)
-{ 
+{
 	while($posts = $result->fetch_assoc())
 	{
-		
+
 		$vars = array(
-			'$postid$'		=> $posts['post_id'],
-			'$posturl$'		=> ($config['use-modrewrite'] == 1) ? $posts['post_slug'] : $config['miniblog-filename'] . '?post=' . $posts['post_slug'],
-			'$posttitle$'	=> stripslashes($posts['post_title']),
-			'$postdate$'	=> date($config['date-format'], $posts['date']),
-			'$postcontent$'	=> stripslashes($posts['post_content']),
+			'$postid$' => $posts['post_id'],
+			'$posturl$' => ($config['use-modrewrite'] == 1) ? $posts['post_slug'] : $config['miniblog-filename'] . '?post=' . $posts['post_slug'],
+			'$posttitle$' => stripslashes($posts['post_title']),
+			'$postdate$' => date($config['date-format'], $posts['date']),
+			'$postcontent$' => stripslashes($posts['post_content']),
 		);
-		
-		$template_vars		= array_keys($vars);
-		$template_values	= array_values($vars);
-		
+
+		$template_vars = array_keys($vars);
+		$template_values = array_values($vars);
+
 		$output = file_get_contents(PATH . 'includes/template.html');
 		$output = str_replace($template_vars, $template_values, $output);
-		
+
 		$miniblog_posts .= $output;
 	}
 }
 
-$single = ($post == '') ? false : true;
+$single = $post != '';
 
 if($total > ($from + $ppp))
 {
