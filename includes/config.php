fb2d4682d2fc19221af351fa1f7b47c024f52783
Graham Holtslander <menello@gmail.com>
2014-06-02 22:06:55 -0600

    Adding sandbox configs

--- includes/config.php
+++ includes/config.php
@@ -4,12 +4,21 @@ if(!defined('IN_BLOG'))
 	exit;
 }
 
+// localhost config options
+// $sqlconfig = array();
+// $sqlconfig['username'] = 'root'; // database username
+// $sqlconfig['password'] = 'root'; // database password
+// $sqlconfig['host'] = 'localhost'; // database host (usually localhost)
+// $sqlconfig['port'] = 8889; // database port
+// $sqlconfig['dbname'] = 'miniblog'; // database name
+
+// sandbox config options
 $sqlconfig = array();
-$sqlconfig['username'] = 'root'; // database username
-$sqlconfig['password'] = 'root'; // database password
+$sqlconfig['username'] = 'sandbox_miniblog'; // database username
+$sqlconfig['password'] = 'Supers3cure!'; // database password
 $sqlconfig['host'] = 'localhost'; // database host (usually localhost)
 $sqlconfig['port'] = 8889; // database port
-$sqlconfig['dbname'] = 'miniblog'; // database name
+$sqlconfig['dbname'] = 'sandbox_miniblog'; // database name
 
 
 ?>
