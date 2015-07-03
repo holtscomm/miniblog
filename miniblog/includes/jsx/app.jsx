b827770f7f67a5eb06a9ce658099139ef2922d1e
Graham Holtslander <graham.holtslander@zu.com>
2015-07-03 08:05:52 -0600

    Add babel, react-router

---
+++ miniblog/includes/jsx/app.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+
+var routes = (
+  <Route handler={App}>
+    <Route path='about' handler='{About}' />
+    <Route path='inbox' handler='{Inbox}' />
+  </Route>
+);
+
+var RouteHandler = Router.RouteHandler;
+
+var App = React.createClass({
+  render: () {
+    return (
+      <div>
+        <h1>App</h1>
+        <RouteHandler />
+      </div>
+    )
+  }
+});
+
+Router.run(routes, Router.HashLocation, (Root) => {
+  React.render(<Root />, document.getElementById('attacher'));
+})
